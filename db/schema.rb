da6d5fa4354375e04d4063c6e2a984de77d6c70f
Milan Pudasini <esror.milan@gmail.com>
2016-05-09 17:53:56 +0545

    Creates the comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509114909) do
+ActiveRecord::Schema.define(version: 20160509120754) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
