bfe1bab6ee2317fe16209eb75246307478a0a2b9
Milan Pudasini <esror.milan@gmail.com>
2016-05-10 15:29:49 +0545

    Adds the show and index methods

--- app/controllers/api/v1/posts.rb
+++ app/controllers/api/v1/posts.rb
@@ -5,10 +5,22 @@ module API
       format :json
 
       resource :posts do
+
         desc "Returns a list of all posts"
         get do
           Post.all
         end
+
+        desc "Returns a specific post"
+        params do
+          requires :id, type: Integer, desc: 'Post id.'
+        end
+        route_param :id do
+          get do
+            Post.find(params[:id])
+          end
+        end
+
       end
 
     end
